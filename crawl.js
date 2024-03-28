a6590c3588a9ca8dcf894506e1702489387a29a2
Steven <134742313+StevenH0824@users.noreply.github.com>
2024-03-28 17:32:54 -0400

    crawl page recursivly

--- crawl.js
+++ crawl.js
@@ -1,24 +1,46 @@
 const{JSDOM} = require('jsdom')
 
-async function crawlPage(currentURL){
+async function crawlPage(baseURL, currentURL, pages){
+    
+    const baseURLObj = new URL(baseURL)
+    const currentURLObj = new URL(currentURL)
+    
+    if (baseURLObj.hostname !== currentURLObj.hostname){
+        return pages
+    }
+
+    const normalizedCurrentURL = normalizeURL(currentURL)
+    if (pages[normalizedCurrentURL] > 0){
+        pages[normalizedCurrentURL]++
+        return pages
+    }
+    
+    pages[normalizedCurrentURL] = 1
+    
     console.log("Active crawling: " + currentURL +".")
     try{
         const resp = await fetch(currentURL)
-        console.log(await resp.text())
         if (resp.status > 399){
             console.log("Error in fetch:\t" + "Status code " + resp.status + "on page " + currentURL)
-            return
+            return pages
         }
-
+        
         const contentType = resp.headers.get("content-type")
         if (!contentType.includes("text/html")){
             console.log("Non html response, content type:" + contentType + " on page " + currentURL)
-            return
+            return pages
         }
+        const htmlBody = await resp.text()
 
+        const nextURLs = getURLsFromHTML(htmlBody,baseURL)
+
+        for (const nextURL of nextURLs){
+            pages = await crawlPage(baseURL,nextURL,pages)
+        }
     } catch (err){
         console.log("Error in fetch:\t" + err.message + " on page " + currentURL)    
     }
+    return pages
 }
 
 
@@ -34,14 +56,14 @@ function getURLsFromHTML(htmlBody, baseURL){
                 const urlObj = new URL(baseURL + linkElement.href)
                 urls.push(urlObj.href)
             } catch (err){
-                console.log("Error with Relative Url" + err.message)
+                console.log("Error with Relative Url:\t" + err.message)
             }
         }else{
             try{
                 const urlObj = new URL(linkElement.href)
                 urls.push(urlObj.href)
             } catch (err){
-                console.log("Error with Absolute Url" + err.message)
+                console.log("Error with Absolute Url:\t" + err.message)
             }
         }
     }
